27a999dae9eeb484486e43a0f7a7747add7d1739
veilwr4ith <114529635+veilwr4ith@users.noreply.github.com>
2024-04-08 06:30:55 +0800

    Create hex.py

---
+++ hex.py
@@ -0,0 +1,17 @@
+def hex_to_plaintext(hex_string):
+    try:
+        hex_string = hex_string.replace(" ", "")
+        byte_data = bytes.fromhex(hex_string)
+        plaintext = byte_data.decode('utf-8')
+        return plaintext
+    except Exception as i:
+        print("Error:", i)
+        return None
+    
+hex_input = input("Enter the hexadecimal string: ")
+
+plaintext = hex_to_plaintext(hex_input)
+if plaintext:
+    print("Plaintext:", plaintext)
+else:
+    print("Failed to decode the hex string.")
