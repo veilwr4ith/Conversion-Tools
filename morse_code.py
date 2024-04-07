a24048c3568a84d0f0be135ac39413257bfe6a23
veilwr4ith <114529635+veilwr4ith@users.noreply.github.com>
2024-04-08 04:30:55 +0800

    Create morse_code.py

---
+++ morse_code.py
@@ -0,0 +1,27 @@
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.',
+    ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-',
+    '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': '/'
+}
+
+def morse_decoder(text):
+    words = text.split('/')
+    decoded_message = []
+    for word in words:
+        letters = word.split()
+        decoded_word = ''
+        for letter in letters:
+            decoded_word += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(letter)]
+        decoded_message.append(decoded_word)
+    return ' '.join(decoded_message)
+
+morse_code = input("Enter the Morse Code: ")
+
+decoded_message = morse_decoder(morse_code)
+print("Decoded Message:", decoded_message)
