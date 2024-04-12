cdd80018e26b26ec207a8da5ce788326f5de7dd6
veilwr4ith <114529635+veilwr4ith@users.noreply.github.com>
2024-04-12 19:55:59 +0800

    Add files via upload

---
+++ ROT13.py
@@ -0,0 +1,14 @@
+def rot13_decoder(text):
+    decoded_text = ""
+    for char in text:
+        if 'A' <= char <= 'Z':
+            decoded_text += chr((ord(char) - ord('A') + 13) % 26 + ord('A'))
+        elif 'a' <= char <= 'z':
+            decoded_text += chr((ord(char) - ord('a') + 13) % 26 + ord('a'))
+        else:
+            decoded_text += char
+    return decoded_text
+
+encoded_text = input("Enter the ROT13 String: ")
+decoded_text = rot13_decoder(encoded_text)
+print(f"Decoded Text: {decoded_text}")
