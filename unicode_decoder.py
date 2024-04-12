77cc5be5076ada47b35f499d98e4f2a4cf42fc48
veilwr4ith <114529635+veilwr4ith@users.noreply.github.com>
2024-04-12 14:51:13 +0800

    Create unicode_decoder.py

---
+++ unicode_decoder.py
@@ -0,0 +1,10 @@
+def unicode_decoder(text):
+    try:
+        decoded_text = text.encode('utf-8').decode('unicode-escape')
+        return decoded_text
+    except UnicodeDecodeError:
+        return "Error: Unable to decode Unicode text"
+      
+unicode_text = input("Enter the Unicode Text: ")
+decoded_text = unicode_decoder(unicode_text)
+print("Decoded text:", decoded_text)
